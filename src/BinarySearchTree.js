a91424f30d10a757503474b06a3411c0281db240
Mark Drastrup Olesen and Stefano <mark.drastrup+steppashr@gmail.com>
2019-07-11 21:48:28 +0900

    Implemented insert value in bst

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,5 +1,30 @@
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+  insert(val) {
+    if (val === this.value) {
+      return this;
+    } else if (val > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(val);
+      } else {
+        this.right.insert(val);
+      }
+    } else if (val < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(val);
+      } else {
+        this.left.insert(val);
+      }
+    }
+    return this;
+  }
+}
 
+module.exports = BinarySearchTree;
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 |X                               X
